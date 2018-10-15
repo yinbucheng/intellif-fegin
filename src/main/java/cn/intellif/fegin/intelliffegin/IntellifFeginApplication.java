f5fc2437c87df1e2740359cc6c855c0d6e79c90d
yinchong <yin.chong@intellif.com>
2018-10-15 20:12:54 +0800

    Initial commit

---
+++ src/main/java/cn/intellif/fegin/intelliffegin/IntellifFeginApplication.java
@@ -0,0 +1,12 @@
+package cn.intellif.fegin.intelliffegin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellifFeginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellifFeginApplication.class, args);
+	}
+}
